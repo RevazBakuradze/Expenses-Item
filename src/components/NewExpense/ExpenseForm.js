8335d7e0d4212402d913014f788e547bbf1651bc
Revaz Bakuradze <60052562+RevazBakuradze@users.noreply.github.com>
2021-08-22 01:34:45 +0400

    Added form submit handler and console log the object created

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,75 +1,94 @@
-import react, { useState } from "react";
-import "./ExpenseForm.css";
+import react, { useState } from 'react';
+import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-  // const [enteredTitle, setEnteredTitle] = useState("");
-  // const [enteredAmount, setEnteredAmount] = useState("");
-  // const [enteredDate, setEnteredDate] = useState("");
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
 
-  const [userInput, setUserInput] = useState({
-    enteredTitle: "",
-    enteredAmount: "",
-    enteredDate: "",
-  });
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate: '',
+  // });
 
   const titleChangeHandler = (event) => {
-    // setEnteredTitle(event.target.value);
+    setEnteredTitle(event.target.value);
     // console.log(enteredTitle);
 
-    setUserInput({
-      ...userInput,
-      enteredTitle: event.target.value
-    });
+    //Not a good practice
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value
+    // });
+
+    // Function form to use when state update depends on the previoust state
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
   };
 
   const amountChangeHandler = (event) => {
-    // setEnteredAmount(event.target.value);
+    setEnteredAmount(event.target.value);
     // console.log(enteredAmount);
 
-    setUserInput({
-      ...userInput,
-      enteredAmount: event.target.value
-    });
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
   };
 
   const dateChangeHandler = (event) => {
-    // setEnteredDate(event.target.value);
+    setEnteredDate(event.target.value);
     // console.log(enteredDate);
 
-    setUserInput({
-      ...userInput,
-      enteredDate: event.target.value
-    });
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault(); //Prevents reload
+    
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    };
+
+    console.log(expenseData);
+    
   };
 
   return (
-    <form>
-      <div className="new-expense__controls">
-        <div className="new-expense__controls">
+    <form onSubmit={submitHandler}>
+      <div className='new-expense__controls'>
+        <div className='new-expense__controls'>
           <lable>Title</lable>
-          <input type="text" onChange={titleChangeHandler} />
+          <input type='text' onChange={titleChangeHandler} />
         </div>
-        <div className="new-expense__controls">
+        <div className='new-expense__controls'>
           <lable>Amount</lable>
           <input
-            type="number"
-            min="0.01"
-            step="0.01"
+            type='number'
+            min='0.01'
+            step='0.01'
             onChange={amountChangeHandler}
           />
         </div>
-        <div className="new-expense__controls">
+        <div className='new-expense__controls'>
           <lable>Date</lable>
           <input
-            type="date"
-            min="2019-01-01"
-            max="2022-12-31"
+            type='date'
+            min='2019-01-01'
+            max='2022-12-31'
             onChange={dateChangeHandler}
           />
         </div>
       </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
+      <div className='new-expense__actions'>
+        <button type='submit'>Add Expense</button>
       </div>
     </form>
   );
